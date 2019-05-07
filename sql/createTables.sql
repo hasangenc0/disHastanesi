e9375cc2d88f9c33f31a8410f8ae7e11fa54ee49
hasan <hasangencx@hotmail.com>
2019-05-07 20:04:41 +0300

    homepage

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,6 +1,19 @@
-create table appoinments 
-( 
-    id serial primary key, 
+/* Users table */
+create table "users"
+(
+  id bigserial primary key NOT NULL,
+  firstname text NOT NULL,
+  lastname text NOT NULL,
+  tcid VARCHAR (11) NOT NULL,
+  email character varying(20) NOT NULL,
+  phone character varying(20) NOT NULL,
+  password text NOT NULL,
+)
+
+/* Appoinments table */
+create table appoinments
+(
+    id serial primary key NOT NULL,
     first_name VARCHAR (99) NOT NULL,
     second_name VARCHAR (99) NOT NULL,
     tcid VARCHAR (99) NOT NULL,
@@ -12,3 +25,4 @@ create table appoinments
     created_on TIMESTAMP NOT NULL,
     last_login TIMESTAMP
 );
+
